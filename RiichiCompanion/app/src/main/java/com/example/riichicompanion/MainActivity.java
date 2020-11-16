f98a29e1862e790a949b5be9697987a36d081ff7
Alexander Steffler <stefflera@yahoo.com>
2020-11-16 11:10:36 -0800

    Changed AlertDialog import to androidx.appcompat

--- RiichiCompanion/app/src/main/java/com/example/riichicompanion/MainActivity.java
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.riichicompanion;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
