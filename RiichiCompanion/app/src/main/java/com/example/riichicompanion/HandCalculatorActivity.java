3994c9832c4a548a0b99c48a6dc79d7a9d5b1c25
Alexander Steffler <stefflera@yahoo.com>
2020-11-23 09:10:51 -0800

    Set HandCalculatorActivity extras

--- RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandCalculatorActivity.java
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandCalculatorActivity.java
@@ -6,24 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 public class HandCalculatorActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 12;
     public static final int RESULT_CODE_CONFIRMED = 24;
     public static final int RESULT_CODE_CANCELLED = 36;
-    public static final String WIN_TYPE_EXTRA = "com.example.riichicompanion.WIN_TYPE_EXTRA";
 
+    public static final String WINNER_NAME_EXTRA = "com.example.riichicompanion.WINNER_NAME_EXTRA";
+    public static final String WIN_TYPE_EXTRA = "com.example.riichicompanion.WIN_TYPE_EXTRA";
+    public static final String PREVALENT_WIND_EXTRA = "com.example.riichicompanion.PREVALENT_WIND_EXTRA";
+    public static final String SEAT_WIND_EXTRA = "com.example.riichicompanion.SEAT_WIND_EXTRA";
+
+    private String winnerName;
     private WinType winType;
+    private Wind prevalentWind;
+    private Wind seatWind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(PersistentStorage.getThemeOption(this).getThemeId());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hand_calculator);
         setSupportActionBar(findViewById(R.id.toolbarHandCalc));
-
-        winType = WinType.valueOf(getIntent().getStringExtra(WIN_TYPE_EXTRA));
+        setExtras(getIntent());
     }
 
     @Override
@@ -37,10 +43,25 @@ public class HandCalculatorActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void confirmHandScore(View view) {
-        Intent data = new Intent();
-        data.putExtra(ScoreTrackerActivity.CALCULATED_HAND_EXTRA, new HandScore(2, 60));
-        setResult(RESULT_CODE_CONFIRMED, data);
-        finish();
+    private void setExtras(Intent intent) {
+        winnerName = intent.getStringExtra(WINNER_NAME_EXTRA);
+
+        try {
+            winType = WinType.valueOf(intent.getStringExtra(WIN_TYPE_EXTRA));
+        } catch (IllegalArgumentException e) {
+            winType = WinType.Unknown;
+        }
+
+        try {
+            prevalentWind = Wind.valueOf(intent.getStringExtra(PREVALENT_WIND_EXTRA));
+        } catch (IllegalArgumentException e) {
+            prevalentWind = Wind.East;
+        }
+
+        try {
+            seatWind = Wind.valueOf(intent.getStringExtra(SEAT_WIND_EXTRA));
+        } catch (IllegalArgumentException e) {
+            seatWind = Wind.East;
+        }
     }
 }
