f19160c233a82ba9b5dea2ab436fff0a15835923
Alexander Steffler <stefflera@yahoo.com>
2020-11-23 11:56:49 -0800

    Recieves honba count

--- RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandCalculatorActivity.java
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandCalculatorActivity.java
@@ -1,11 +1,11 @@
 package com.example.riichicompanion;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
@@ -19,11 +19,13 @@ public class HandCalculatorActivity extends AppCompatActivity {
     public static final String WIN_TYPE_EXTRA = "com.example.riichicompanion.WIN_TYPE_EXTRA";
     public static final String PREVALENT_WIND_EXTRA = "com.example.riichicompanion.PREVALENT_WIND_EXTRA";
     public static final String SEAT_WIND_EXTRA = "com.example.riichicompanion.SEAT_WIND_EXTRA";
+    public static final String HONBA_EXTRA = "com.example.riichicompanion.HONBA_EXTRA";
 
     private String winnerName;
     private WinType winType;
     private Wind prevalentWind;
     private Wind seatWind;
+    private int honba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +37,19 @@ public class HandCalculatorActivity extends AppCompatActivity {
 
         if (winnerName != null)
             setTitle(String.format(Locale.getDefault(), "Calculate hand for %s", winnerName));
+
+        // Set views if winType != null
+
+        // Set views if prevalentWind != null
+
+        // Set views if seatWind != null
+
+        // Set views if honba == -1
     }
 
     private void setExtras(Intent intent) {
         winnerName = intent.getStringExtra(WINNER_NAME_EXTRA);
+        honba = intent.getIntExtra(HONBA_EXTRA, -1);
 
         try {
             winType = WinType.valueOf(intent.getStringExtra(WIN_TYPE_EXTRA));
@@ -51,14 +62,14 @@ public class HandCalculatorActivity extends AppCompatActivity {
             prevalentWind = Wind.valueOf(intent.getStringExtra(PREVALENT_WIND_EXTRA));
         }
         catch (Exception e) {
-            prevalentWind = Wind.East;
+            prevalentWind = null;
         }
 
         try {
             seatWind = Wind.valueOf(intent.getStringExtra(SEAT_WIND_EXTRA));
         }
         catch (Exception e) {
-            seatWind = Wind.East;
+            seatWind = null;
         }
     }
 
