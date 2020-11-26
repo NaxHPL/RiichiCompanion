697fe44402f720933bdae4af5e7850b598394dd6
Alexander Steffler <stefflera@yahoo.com>
2020-11-25 21:52:54 -0800

    Added HandInputFragment and HandInputViewModel

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandInputFragment.java
@@ -0,0 +1,36 @@
+package com.example.riichicompanion;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HandInputFragment extends Fragment {
+
+    private HandInputViewModel mViewModel;
+
+    public static HandInputFragment newInstance() {
+        return new HandInputFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.hand_input_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HandInputViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandInputViewModel.java
@@ -0,0 +1,7 @@
+package com.example.riichicompanion;
+
+import androidx.lifecycle.ViewModel;
+
+public class HandInputViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
