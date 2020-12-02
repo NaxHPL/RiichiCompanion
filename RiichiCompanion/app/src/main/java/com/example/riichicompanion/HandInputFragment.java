1d9f893fa93eb61c7e1e2e8284afbeb4e21bdc9f
Alexander Steffler <stefflera@yahoo.com>
2020-12-01 17:34:41 -0800

    Only one of chii/pon/kan/closed kan can be selected at a time

--- RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandInputFragment.java
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/HandInputFragment.java
@@ -1,5 +1,6 @@
 package com.example.riichicompanion;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,18 +12,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class HandInputFragment extends Fragment {
 
     private HandInputViewModel mViewModel;
+
+    //region Views
+
+    ConstraintLayout clExposedTiles;
+    ConstraintLayout clConcealedTiles;
+    ToggleButton tbChii;
+    ToggleButton tbPon;
+    ToggleButton tbKan;
+    ToggleButton tbClosedKan;
+
+    //endregion
 
     public static HandInputFragment newInstance() {
         return new HandInputFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_hand_input, container, false);
     }
 
@@ -33,4 +46,52 @@ public class HandInputFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //region Views
+
+        clExposedTiles = view.findViewById(R.id.clExposedTiles);
+        clConcealedTiles = view.findViewById(R.id.clConcealedTiles);
+        tbChii = view.findViewById(R.id.tbChii);
+        tbPon = view.findViewById(R.id.tbPon);
+        tbKan = view.findViewById(R.id.tbKan);
+        tbClosedKan = view.findViewById(R.id.tbClosedKan);
+
+        //endregion
+
+        setButtonListeners();
+    }
+
+    private void setButtonListeners() {
+        tbChii.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tbPon.setChecked(false);
+                tbKan.setChecked(false);
+                tbClosedKan.setChecked(false);
+            }
+        });
+        tbPon.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tbChii.setChecked(false);
+                tbKan.setChecked(false);
+                tbClosedKan.setChecked(false);
+            }
+        });
+        tbKan.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tbChii.setChecked(false);
+                tbPon.setChecked(false);
+                tbClosedKan.setChecked(false);
+            }
+        });
+        tbClosedKan.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tbChii.setChecked(false);
+                tbPon.setChecked(false);
+                tbKan.setChecked(false);
+            }
+        });
+    }
 }
