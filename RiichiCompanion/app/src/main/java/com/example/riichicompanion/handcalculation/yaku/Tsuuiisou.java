a8b90ab453ab77a4626127c74e6e28b8f9f1854b
Alexander Steffler <stefflera@yahoo.com>
2020-12-14 20:47:22 -0800

    Add Tsuuiisou yakuman

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Tsuuiisou.java
@@ -0,0 +1,46 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Tsuuiisou implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, WinConditions conditions) {
+        int[] tileCounts = hand.getTileCountsClone();
+
+        for (int i = 0; i <= 26; i++) {
+            if (tileCounts[i] > 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int getYakumans() {
+        return 1;
+    }
+
+    @Override
+    public int getClosedHan() {
+        return 0;
+    }
+
+    @Override
+    public int getOpenHan() {
+        return 0;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Tsuuiisou";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
