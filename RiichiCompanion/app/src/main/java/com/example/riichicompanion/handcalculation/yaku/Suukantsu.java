3df93b93b2c0e27ed6f4b4ccdf9d2d9b091ad9b1
Alexander Steffler <stefflera@yahoo.com>
2020-12-14 21:51:42 -0800

    Added Suukantsu yakuman

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Suukantsu.java
@@ -0,0 +1,50 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.Meld;
+import com.example.riichicompanion.handcalculation.MeldType;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Suukantsu implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, WinConditions conditions) {
+        int kanCount = 0;
+
+        for (Meld meld : hand.getMelds()) {
+            if (meld.getMeldType() != MeldType.Kan || meld.getMeldType() != MeldType.ClosedKan)
+                return false;
+
+            kanCount++;
+        }
+
+        return kanCount == 4;
+    }
+
+    @Override
+    public int getYakumans() {
+        return 1;
+    }
+
+    @Override
+    public int getClosedHan() {
+        return 0;
+    }
+
+    @Override
+    public int getOpenHan() {
+        return 0;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Suukantsu";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
