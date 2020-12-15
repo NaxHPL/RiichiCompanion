2831c24830439563713418dcd04a0bfd5abc76b2
Alexander Steffler <stefflera@yahoo.com>
2020-12-14 20:40:34 -0800

    Added Shousuushii and Daisuushii yakuman

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Shousuushii.java
@@ -0,0 +1,50 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Shousuushii implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, WinConditions conditions) {
+        int[] tileCounts = hand.getTileCountsClone();
+        int windTripletsFound = 0;
+        boolean windPairFound = false;
+
+        for (int i = 27; i <= 30; i++) {
+            if (tileCounts[i] >= 3)
+                windTripletsFound++;
+            else if (tileCounts[i] == 2)
+                windPairFound = true;
+        }
+
+        return windPairFound && windTripletsFound == 3;
+    }
+
+    @Override
+    public int getYakumans() {
+        return 1;
+    }
+
+    @Override
+    public int getClosedHan() {
+        return 0;
+    }
+
+    @Override
+    public int getOpenHan() {
+        return 0;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Shousuushii";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
