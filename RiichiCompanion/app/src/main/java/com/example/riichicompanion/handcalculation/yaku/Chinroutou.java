88c6d2ba61175dc9fc9017573168c7a105278f1b
Alexander Steffler <stefflera@yahoo.com>
2020-12-14 20:56:06 -0800

    Added Chinroutou yakuman

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Chinroutou.java
@@ -0,0 +1,48 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Chinroutou implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, WinConditions conditions) {
+        int[] tileCounts = hand.getTileCountsClone();
+
+        for (int i = 0; i < tileCounts.length; i++) {
+            if (i != 0 && i != 8 && i != 9 && i != 17 && i != 18 && i != 26) {
+                if (tileCounts[i] > 0)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int getYakumans() {
+        return 1;
+    }
+
+    @Override
+    public int getClosedHan() {
+        return 0;
+    }
+
+    @Override
+    public int getOpenHan() {
+        return 0;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Chinroutou";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
