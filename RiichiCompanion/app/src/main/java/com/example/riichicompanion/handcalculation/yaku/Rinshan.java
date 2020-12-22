ec1b928dc8fc25690bda2dc6770a1cd29ab00a9b
Alexander Steffler <stefflera@yahoo.com>
2020-12-22 10:20:25 -0800

    Added Rinshan kaihou yaku

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Rinshan.java
@@ -0,0 +1,35 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.HandArrangement;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Rinshan implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, HandArrangement arrangement, WinConditions conditions) {
+        return conditions.isRinshan();
+    }
+
+    @Override
+    public int getYakumans() {
+        return 0;
+    }
+
+    @Override
+    public int getHan() {
+        return 1;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Rinshan kaihou";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
