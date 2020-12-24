8b3c1cdf3c1aabc0c6f16b37a8e6475a55b0b5c2
Alexander Steffler <stefflera@yahoo.com>
2020-12-23 20:22:40 -0800

    Added Honroutou yaku

---
+++ RiichiCompanion/app/src/main/java/com/example/riichicompanion/handcalculation/yaku/Honroutou.java
@@ -0,0 +1,44 @@
+package com.example.riichicompanion.handcalculation.yaku;
+
+import com.example.riichicompanion.handcalculation.Hand;
+import com.example.riichicompanion.handcalculation.HandArrangement;
+import com.example.riichicompanion.handcalculation.WinConditions;
+
+import java.util.ArrayList;
+
+public class Honroutou implements Yaku {
+
+    @Override
+    public boolean isConditionMet(Hand hand, HandArrangement arrangement, WinConditions conditions) {
+        int[] tileCounts = hand.getTileCountsClone();
+
+        for (int i = 0; i < tileCounts.length; i++) {
+            if ((i >= 1 && i <= 7) || (i >= 10 && i <= 16) || (i >= 19 && i <= 25)) {
+                if (tileCounts[i] > 0)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int getYakumans() {
+        return 0;
+    }
+
+    @Override
+    public int getHan() {
+        return 2;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Honroutou";
+    }
+
+    @Override
+    public ArrayList<Class<? extends Yaku>> getInvalidYaku() {
+        return null;
+    }
+}
